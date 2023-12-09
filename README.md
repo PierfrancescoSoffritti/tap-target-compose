f53caf31c4b65c4a9404f720661b99b78bfbea8a
Pierfrancesco Soffritti <psoffritti@google.com>
2023-12-09 17:03:55 +0000

    Update project name

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# TapTargetCompose
+# tap-target-compose
 A compose implementation of Material Design tap targets, for feature discovery.
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,6 @@ dependencyResolutionManagement {
   }
 }
 
-rootProject.name = "TapTargetCompose"
+rootProject.name = "tap-target-compose"
 include(":sample-app")
 include(":tap-target-compose")
