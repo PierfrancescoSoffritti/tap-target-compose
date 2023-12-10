1d88e7546cc0bb2d756f7cf0ce9c72c11c78c84d
PierfrancescoSoffritti <soffritti.pierfrancesco@gmail.com>
2023-12-10 06:00:33 +0000

    Version bump

--- buildSrc/src/main/java/com/psoffritti/taptargetcompose/Configuration.kt
+++ buildSrc/src/main/java/com/psoffritti/taptargetcompose/Configuration.kt
@@ -7,7 +7,7 @@ object Configuration {
   const val minSdkSampleApp = 21
   private const val majorVersion = 0
   private const val minorVersion = 0
-  private const val patchVersion = 1
+  private const val patchVersion = 2
   const val versionCode = 1
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
   const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
