7fc783e1c321f13ea8f94d16fb05b2c917ef3a75
PierfrancescoSoffritti <soffritti.pierfrancesco@gmail.com>
2024-05-22 19:41:21 +0100

    Update to version 1.1.2

--- buildSrc/src/main/java/com/psoffritti/taptargetcompose/Configuration.kt
+++ buildSrc/src/main/java/com/psoffritti/taptargetcompose/Configuration.kt
@@ -7,7 +7,7 @@ object Configuration {
   const val minSdkSampleApp = 21
   private const val majorVersion = 1
   private const val minorVersion = 1
-  private const val patchVersion = 1
+  private const val patchVersion = 2
   const val versionCode = 3
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
   const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
